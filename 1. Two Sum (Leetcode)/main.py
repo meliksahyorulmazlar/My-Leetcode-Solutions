1253bcf4835031e4778d95f4097cc43b629099f0
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-05-27 07:45:04 -0700

    Add files via upload

---
+++ 1. Two Sum (Leetcode)/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        results = []
+        for i in range(len(List)):
+            for j in range(i,len(List)):
+                if List[i] + List[j] == target:
+                    return [i,j]
+
+
+
