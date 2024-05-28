5dbabae1d7e7914fb6316b0e19311348324c51eb
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-05-28 09:01:50 -0700

    Add files via upload

---
+++ 28. Find the Index of the First Occurrence in a String (Leetcode)/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)):
+            for j in range(len(needle)):
+                if haystack[i]
+
+
+
+sol = Solution()
+sol.strStr("sadbutsad","sad")
+
+
