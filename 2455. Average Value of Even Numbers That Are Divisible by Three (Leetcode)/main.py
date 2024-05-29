7694716fd3be0da60b12c745d88cf23ad47b8654
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-05-29 14:22:00 -0700

    Add files via upload

---
+++ 2455. Average Value of Even Numbers That Are Divisible by Three (Leetcode)/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        nums = [num for num in nums if num % 6 == 0]
+        try:
+            return int(sum(nums)/len(nums))
+        except ZeroDivisionError:
+            return 0
