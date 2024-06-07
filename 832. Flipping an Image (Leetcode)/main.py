16be86c67343ad599ab3d53c32f9d03847b0abdb
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-06-07 16:42:18 -0700

    Add files via upload

---
+++ 832. Flipping an Image (Leetcode)/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i] = image[i][::-1]
+
+        for i in range(len(image)):
+            for j in range(len(image[i])):
+                if image[i][j] == 0:
+                    image[i][j] = 1
+                else:
+                    image[i][j] = 0
+        return image   
