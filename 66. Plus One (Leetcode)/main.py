c583dee3c7720796711da9d499801d73f7492093
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-06-10 17:03:28 -0700

    Add files via upload

--- 66. Plus One (Leetcode)/main.py
+++ 66. Plus One (Leetcode)/main.py
@@ -1,10 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        total = 0
-        digits = digits[::-1]
-        for i in range(len(digits)):
-            digit = digits[i]
-            total += digit*pow(10,i)
-        total += 1
-        total = list(str(total))
-        return [int(item) for item in total]
+        digits = [str(d) for d in digits]
+        number = int("".join(digits))
+        number = str(number + 1)
+        return [int(n) for n in list(number)]
