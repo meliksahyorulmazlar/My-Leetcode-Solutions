8e3ac572164b591f59d210c23d3c28486e191a7f
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-02-23 06:08:00 -0500

    Update main.py

--- 283. Move Zeroes (Leetcode)/main.py
+++ 283. Move Zeroes (Leetcode)/main.py
@@ -1,7 +1,17 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        for num in nums:
-            if num == 0:
-                nums.remove(num)
-                nums.append(0)
-        return nums
+        # My first solution
+        # for num in nums:
+        #     if num == 0:
+        #         nums.remove(num)
+        #         nums.append(0)
+        # return nums
+
+        #Efficient Solution- My new solution
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+        for i in range(j,len(nums)):
+            nums[i] = 0
