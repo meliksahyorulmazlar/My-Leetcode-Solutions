cff3892066821ff4d3f7d7066bca1c64bdb592a8
Meliksah Yorulmazlar <yorulk@rpi.edu>
2025-07-08 06:47:31 -0400

    Update main2.py
    added comments

--- 1. Two Sum (Leetcode)/main2.py
+++ 1. Two Sum (Leetcode)/main2.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        # This code finds the complement
         complements = {}
 
         for i, num in enumerate(nums):
